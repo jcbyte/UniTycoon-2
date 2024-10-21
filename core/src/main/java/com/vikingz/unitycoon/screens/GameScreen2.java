b46876e7b1deba06814b78fd2a43aeb16cf66ade
Ren <renfoxlover@gmail.com>
2024-10-21 14:02:28 +0100

    Revert "Revert "Fixes settings layout""
    This reverts commit ee74d227cf7fd8c56cde335e0f019a34c7722185.

---
+++ core/src/main/java/com/vikingz/unitycoon/screens/GameScreen2.java
@@ -0,0 +1,10 @@
+package com.vikingz.unitycoon.screens;
+
+import com.badlogic.gdx.Game;
+import com.vikingz.unitycoon.global.GameSkins;
+
+public class GameScreen2 {
+    public GameScreen2(Game game, String mapName, GameSkins SkinLoader){
+
+    }
+}
