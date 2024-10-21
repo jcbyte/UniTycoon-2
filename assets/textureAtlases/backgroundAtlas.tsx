54889ffbd107b0cd418bb0b6e9f6f337d01c07ec
Ren <Fox@yae.gay>
2024-10-21 17:25:40 +0100

    Adds config files

---
+++ assets/textureAtlases/backgroundAtlas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="backgroundAtlas" tilewidth="32" tileheight="32" tilecount="40" columns="10">
+ <image source="../textureAtlases/backgroundAtlas.png" width="320" height="128"/>
+</tileset>
