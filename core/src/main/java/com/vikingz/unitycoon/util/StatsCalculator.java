1c8d64677f1098ba5c4a2692bf8793bdaee7d144
Ren <renfoxlover@gmail.com>
2024-10-21 14:06:38 +0100

    Test

--- core/src/main/java/com/vikingz/unitycoon/util/StatsCalculator.java
+++ core/src/main/java/com/vikingz/unitycoon/util/StatsCalculator.java
@@ -3,7 +3,7 @@ package com.vikingz.unitycoon.util;
 public class StatsCalculator {
 
     public static int calculateSatisfaction(int amountOfStudents, float satisfactionMultiplier){
-
+        //Test if git is broken
         return Math.round(amountOfStudents * satisfactionMultiplier);
 
     }
