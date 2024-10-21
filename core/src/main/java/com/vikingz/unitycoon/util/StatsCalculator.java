00cae3f2455a268fd5c01274c485bd2a5f3506a7
Ren <renfoxlover@gmail.com>
2024-10-21 14:07:20 +0100

    yippie I hate github.com

--- core/src/main/java/com/vikingz/unitycoon/util/StatsCalculator.java
+++ core/src/main/java/com/vikingz/unitycoon/util/StatsCalculator.java
@@ -3,7 +3,6 @@ package com.vikingz.unitycoon.util;
 public class StatsCalculator {
 
     public static int calculateSatisfaction(int amountOfStudents, float satisfactionMultiplier){
-        //Test if git is broken
         return Math.round(amountOfStudents * satisfactionMultiplier);
 
     }
