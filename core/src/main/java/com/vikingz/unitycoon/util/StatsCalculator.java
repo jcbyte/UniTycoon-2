5bb9d6db5218093add00a2f0b5195d100ee6bb58
Ren <renfoxlover@gmail.com>
2024-10-21 14:02:22 +0100

    yippie I hate github.com
    Test
    
    Revert "Revert "Renames it to correct spelling""
    
    This reverts commit 1f301a1a8218388a6c106271a69b4b1a42f2bbd8.
    
    Revert "Renames it to correct spelling"
    
    This reverts commit 9eaba94e8d25b7b1cd2a651e59bbb22f36c42dad.
    
    Revert "Revert "Fixes settings layout""
    
    This reverts commit ee74d227cf7fd8c56cde335e0f019a34c7722185.
    
    Revert "Fixes settings layout"
    
    This reverts commit a4cfc7f5e838369448771b7383e7dc9bb9154d08.

--- core/src/main/java/com/vikingz/unitycoon/util/StatsCalculator.java
+++ core/src/main/java/com/vikingz/unitycoon/util/StatsCalculator.java
@@ -3,7 +3,6 @@ package com.vikingz.unitycoon.util;
 public class StatsCalculator {
 
     public static int calculateSatisfaction(int amountOfStudents, float satisfactionMultiplier){
-
         return Math.round(amountOfStudents * satisfactionMultiplier);
 
     }
