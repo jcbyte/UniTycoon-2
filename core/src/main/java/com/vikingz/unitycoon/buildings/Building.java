bdac1076c4c540d1b6a2dbaa7afa63bcffbaf763
Ren <renfoxlover@gmail.com>
2024-10-25 13:18:32 +0100

    adds building abstract

--- core/src/main/java/com/vikingz/unitycoon/buildings/Building.java
+++ core/src/main/java/com/vikingz/unitycoon/buildings/Building.java
@@ -1,17 +1,19 @@
 package com.vikingz.unitycoon.buildings;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 
 public abstract class Building {
-
     private Texture textureBuilding;
     private float x;
     private float y;
+    private int width;
+    private int height;
 
 
-
-    public void building(String ImageAsset){
-        this.textureBuilding = new Texture(ImageAsset);
+    public void building(int width, int height){
+        this.width = width;
+        this.height = height;
     }
 
     public Texture getTextureBuilding() {
@@ -20,5 +22,21 @@ public abstract class Building {
 
     public void setTextureBuilding(Texture textureBuilding) {
         this.textureBuilding = textureBuilding;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
     }
 }
