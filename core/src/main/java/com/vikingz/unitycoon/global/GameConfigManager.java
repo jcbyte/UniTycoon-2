eb414308d889ce2485204ce52339024d63caef62
Joel Cutler <joelcutler108@gmail.com>
2024-12-06 16:42:12 +0000

    changed config loader and saver to use json in player prefs

--- core/src/main/java/com/vikingz/unitycoon/global/GameConfigManager.java
+++ core/src/main/java/com/vikingz/unitycoon/global/GameConfigManager.java
@@ -8,6 +8,8 @@ import java.io.ObjectInputStream;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.utils.Json;
 
 
 /**
@@ -44,25 +46,28 @@ public class GameConfigManager {
 
 
 
+    // todo load and save still doesnt seem to be working
     /**
      * Saves GameConfig Object to binary file,
      * to save settings and high score.
      */
     public static void saveGameConfig(){
-        try {
-            FileOutputStream fileOut = new FileOutputStream("config/gameconf.bin");
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-
-            out.writeObject(GameConfig.getInstance());
-            out.close();
-            fileOut.close();
-            System.out.println("Serialized data is saved.");
-
-        } catch (IOException i) {
-            System.out.println("FILE NOT FOUND");
-        }
-
-
+//        try {
+//            FileOutputStream fileOut = new FileOutputStream("config/gameconf.bin");
+//            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+//
+//            out.writeObject(GameConfig.getInstance());
+//            out.close();
+//            fileOut.close();
+//            System.out.println("Serialized data is saved.");
+//
+//        } catch (IOException i) {
+//            System.out.println("FILE NOT FOUND");
+//        }
+        Json json = new Json();
+        String configString = json.toJson(GameConfig.getInstance());
+        Preferences prefs = Gdx.app.getPreferences("prefs");
+        prefs.putString("config", configString);
     }
 
 
@@ -71,29 +76,45 @@ public class GameConfigManager {
      * to load existing settings and high score.
      */
     public static void loadGameConfig(){
-
         GameConfig conf;
-        try {
-            FileInputStream fileIn = new FileInputStream("config/gameconf.bin");
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            conf = (GameConfig) in.readObject();
-            in.close();
-            fileIn.close();
 
-            GameConfig.getInstance().setInstance(conf);
-            System.out.println("\n\nLoaded GameConfig");
-            System.out.println("Music_Volume: " + GameConfig.getInstance().MusicVolumeValue);
-            System.out.println("Song_Volume: " + GameConfig.getInstance().SoundVolumeValue);
+        Json json = new Json();
+        Preferences prefs = Gdx.app.getPreferences("prefs");
 
-
-
-        } catch (IOException i) {
-            System.out.println("FILE NOT FOUND");
-            //i.printStackTrace();
-        } catch (ClassNotFoundException c) {
-            System.out.println("GameConfig class not found");
-            //c.printStackTrace();
+        String configString = prefs.getString("config");
+        if (configString.isEmpty()) {
+            System.out.println("Saved config not found");
+            return;
         }
+        conf = json.fromJson(GameConfig.class, configString);
+        if (conf == null) {
+            System.err.println("Error demoralising config");
+            return;
+        }
+
+        GameConfig.getInstance().setInstance(conf);
+
+//        try {
+//            FileInputStream fileIn = new FileInputStream("config/gameconf.bin");
+//            ObjectInputStream in = new ObjectInputStream(fileIn);
+//            conf = (GameConfig) in.readObject();
+//            in.close();
+//            fileIn.close();
+//
+//            GameConfig.getInstance().setInstance(conf);
+//            System.out.println("\n\nLoaded GameConfig");
+//            System.out.println("Music_Volume: " + GameConfig.getInstance().MusicVolumeValue);
+//            System.out.println("Song_Volume: " + GameConfig.getInstance().SoundVolumeValue);
+//
+//
+//
+//        } catch (IOException i) {
+//            System.out.println("FILE NOT FOUND");
+//            //i.printStackTrace();
+//        } catch (ClassNotFoundException c) {
+//            System.out.println("GameConfig class not found");
+//            //c.printStackTrace();
+//        }
     }
 
 
