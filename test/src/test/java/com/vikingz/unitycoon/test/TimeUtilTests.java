28b7e6c48db9a67e40ff349fb5778267b088b962
Joel Cutler <joelcutler108@gmail.com>
2024-12-21 19:43:24 +0000

    converted random tests into useful TimeUtils tests

---
+++ test/src/test/java/com/vikingz/unitycoon/test/TimeUtilTests.java
@@ -0,0 +1,32 @@
+package com.vikingz.unitycoon.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.vikingz.unitycoon.util.TimeUtil;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests checking {@link TimeUtil}.
+ */
+public class TimeUtilTests extends AbstractHeadlessGdxTest {
+  @Test
+  public void testValidSecondsToMinutes() {
+    assertEquals(new TimeUtil.Time(4, 2), TimeUtil.secondsToMinSecs(124));
+    assertEquals(new TimeUtil.Time(0, 0), TimeUtil.secondsToMinSecs(0));
+    assertEquals(new TimeUtil.Time(48, 157), TimeUtil.secondsToMinSecs(9468));
+  }
+
+  @Test
+  public void testValidSecondsToMinutesString() {
+    assertEquals("01:01", TimeUtil.secondsToMinSecs(61).toString());
+    assertEquals("00:00", TimeUtil.secondsToMinSecs(0).toString());
+    assertEquals("1315:16", TimeUtil.secondsToMinSecs(78916).toString());
+  }
+
+  @Test
+  public void testInvalidSecondsToMinutes() {
+    assertThrows(IllegalArgumentException.class, () -> TimeUtil.secondsToMinSecs(-7));
+  }
+}
