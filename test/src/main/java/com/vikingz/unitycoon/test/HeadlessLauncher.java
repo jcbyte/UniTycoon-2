b1ec36c3a1a9713777ce01bfbb974ed2d8e79c9a
Joel Cutler <joelcutler108@gmail.com>
2024-12-21 18:55:21 +0000

    create headless launcher

--- test/src/main/java/com/vikingz/unitycoon/test/HeadlessLauncher.java
+++ test/src/main/java/com/vikingz/unitycoon/test/HeadlessLauncher.java
@@ -1,7 +1,28 @@
 package com.vikingz.unitycoon.test;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.vikingz.unitycoon.Main;
+
+/**
+ * Launches the headless application.
+ */
 public class HeadlessLauncher {
   public static void main(String[] args) {
-    System.out.println("Headless Launcher");
+    createApplication();
+  }
+
+  private static Application createApplication() {
+    // Note: you can use a custom ApplicationListener implementation for the headless project instead of Gemo.
+    return new HeadlessApplication(new Main(), getDefaultConfiguration());
+  }
+
+  private static HeadlessApplicationConfiguration getDefaultConfiguration() {
+    HeadlessApplicationConfiguration configuration = new HeadlessApplicationConfiguration();
+    // When this value is negative, render() is never called.
+    configuration.updatesPerSecond = -1;
+
+    return configuration;
   }
 }
