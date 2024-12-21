d20dc3c42d49b5d58ed4c2caddebfb2354700990
Joel Cutler <joelcutler108@gmail.com>
2024-12-21 18:55:40 +0000

    create boilerplate test

---
+++ test/src/test/java/com/vikingz/unitycoon/test/RandomTest.java
@@ -0,0 +1,12 @@
+package com.vikingz.unitycoon.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RandomTest extends AbstractHeadlessGdxTest {
+  @Test
+  public void testUniverse() {
+    assertTrue(true);
+  }
+}
