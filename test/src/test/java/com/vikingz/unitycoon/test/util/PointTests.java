4b1ce7f240021a376ff75ceb1dd7bce62962d0f7
Joel Cutler <joelcutler108@gmail.com>
2025-01-07 16:53:25 +0100

    fixed checkstlye issues

--- test/src/test/java/com/vikingz/unitycoon/test/util/PointTests.java
+++ test/src/test/java/com/vikingz/unitycoon/test/util/PointTests.java
@@ -1,7 +1,6 @@
 package com.vikingz.unitycoon.test.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.vikingz.unitycoon.test.AbstractHeadlessGdxTest;
 import com.vikingz.unitycoon.util.Point;
@@ -9,20 +8,20 @@ import org.junit.jupiter.api.Test;
 
 public class PointTests extends AbstractHeadlessGdxTest {
 
-    @Test
-    public void testPointGet(){
-        Point thing = new Point(4,10);
-        assertEquals(thing.getX(), 4);
-        assertEquals(thing.getY(), 10);
-    }
+  @Test
+  public void testPointGet() {
+    Point thing = new Point(4, 10);
+    assertEquals(thing.getX(), 4);
+    assertEquals(thing.getY(), 10);
+  }
 
-    @Test
-    public void testPointSet(){
-        Point thing = new Point(0,0);
-        thing.setX(5);
-        thing.setY(10);
-        assertEquals(thing.getX(), 5);
-        assertEquals(thing.getY(), 10);
-   }
-    
+  @Test
+  public void testPointSet() {
+    Point thing = new Point(0, 0);
+    thing.setX(5);
+    thing.setY(10);
+    assertEquals(thing.getX(), 5);
+    assertEquals(thing.getY(), 10);
+  }
+
 }
