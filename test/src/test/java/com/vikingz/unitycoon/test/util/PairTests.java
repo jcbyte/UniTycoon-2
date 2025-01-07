6cc042a00cab34f64926be5bdb40065f66574279
Joel Cutler <joelcutler108@gmail.com>
2025-01-07 17:03:36 +0100

    fix checkstyle issues

--- test/src/test/java/com/vikingz/unitycoon/test/util/PairTests.java
+++ test/src/test/java/com/vikingz/unitycoon/test/util/PairTests.java
@@ -1,19 +1,19 @@
 package com.vikingz.unitycoon.test.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.vikingz.unitycoon.test.AbstractHeadlessGdxTest;
 import com.vikingz.unitycoon.util.Pair;
 import org.junit.jupiter.api.Test;
 
-
+/**
+ * Tests checking {@link Pair}.
+ */
 public class PairTests extends AbstractHeadlessGdxTest {
-    
-    @Test
-    public void TestPair(){
-        Pair pair = new Pair(11,15);
-        assertEquals(pair.first, 11);
-        assertEquals(pair.second, 15);
-    }
+  @Test
+  public void testPair() {
+    Pair pair = new Pair(11, 15);
+    assertEquals(pair.first, 11);
+    assertEquals(pair.second, 15);
+  }
 }
