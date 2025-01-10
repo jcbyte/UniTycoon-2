e6f934547e2d0afe25dd3172ff7f2775f27da17f
Joel Cutler <joelcutler108@gmail.com>
2025-01-10 18:43:29 +0100

    optimised imports for checkstyle

--- core/src/main/java/com/vikingz/unitycoon/building/BuildingStats.java
+++ core/src/main/java/com/vikingz/unitycoon/building/BuildingStats.java
@@ -3,7 +3,6 @@ package com.vikingz.unitycoon.building;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import java.util.ArrayList;
 import java.util.Dictionary;
 
