89d88d0d538d2195f190a003909d6265a2e6cce0
Joel Cutler <joelcutler108@gmail.com>
2025-01-10 23:19:26 +0100

    added boilerplate game music tests

---
+++ test/src/test/java/com/vikingz/unitycoon/test/audio/GameMusicTests.java
@@ -0,0 +1,9 @@
+package com.vikingz.unitycoon.test.audio;
+
+import com.vikingz.unitycoon.test.AbstractHeadlessGdxTest;
+
+/**
+ * Tests checking {@link com.vikingz.unitycoon.audio.GameMusic} .
+ */
+public class GameMusicTests extends AbstractHeadlessGdxTest {
+}
