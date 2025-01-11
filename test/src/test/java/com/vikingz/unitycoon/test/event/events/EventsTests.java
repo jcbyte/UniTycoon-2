a54f5bf3ca69ea8ead665d684a7a0a1453df5ab4
Joel Cutler <joelcutler108@gmail.com>
2025-01-11 21:23:20 +0000

    test all events options

--- test/src/test/java/com/vikingz/unitycoon/test/event/events/EventsTests.java
+++ test/src/test/java/com/vikingz/unitycoon/test/event/events/EventsTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import com.vikingz.unitycoon.event.Event;
 import com.vikingz.unitycoon.event.events.AlumniVisitEvent;
 import com.vikingz.unitycoon.event.events.BigOilEvent;
 import com.vikingz.unitycoon.event.events.CasinoEvent;
@@ -19,11 +20,13 @@ import com.vikingz.unitycoon.event.events.PeacefulProtestEvent;
 import com.vikingz.unitycoon.event.events.ScolarEvent;
 import com.vikingz.unitycoon.event.events.StrikeEvent;
 import com.vikingz.unitycoon.event.events.UniPartyEvent;
+import com.vikingz.unitycoon.global.GameConfig;
 import com.vikingz.unitycoon.render.BuildingRenderer;
 import com.vikingz.unitycoon.render.GameRenderer;
 import com.vikingz.unitycoon.screens.GameScreen;
 import com.vikingz.unitycoon.test.AbstractHeadlessGdxTest;
 import org.junit.jupiter.api.Test;
+import java.util.List;
 
 /**
  * Tests checking all Events in the {@link com.vikingz.unitycoon.event.events} package.
@@ -37,20 +40,34 @@ public class EventsTests extends AbstractHeadlessGdxTest {
     when(gameScreenMock.getGameRenderer()).thenReturn(gameRendererMock);
     when(gameRendererMock.getBuildingRenderer()).thenReturn(buildingRendererMock);
 
-    assertNotNull(new AlumniVisitEvent(gameScreenMock).getEvent());
-    assertNotNull(new BigOilEvent(gameScreenMock).getEvent());
-    assertNotNull(new CasinoEvent(gameScreenMock).getEvent());
-    assertNotNull(new ChampionshipWinEvent(gameScreenMock).getEvent());
-    assertNotNull(new CurriculumChangeEvent(gameScreenMock).getEvent());
-    assertNotNull(new EarthquakeEvent(gameScreenMock).getEvent());
-    assertNotNull(new ExamWeekEvent(gameScreenMock).getEvent());
-    assertNotNull(new FloodingEvent(gameScreenMock).getEvent());
-    assertNotNull(new GraduationEvent(gameScreenMock).getEvent());
-    assertNotNull(new GrantEvent(gameScreenMock).getEvent());
-    assertNotNull(new LongboiDeathEvent(gameScreenMock).getEvent());
-    assertNotNull(new PeacefulProtestEvent(gameScreenMock).getEvent());
-    assertNotNull(new ScolarEvent(gameScreenMock).getEvent());
-    assertNotNull(new StrikeEvent(gameScreenMock).getEvent());
-    assertNotNull(new UniPartyEvent(gameScreenMock).getEvent());
+    List<Class<? extends Event>> events = List.of(
+        AlumniVisitEvent.class,
+        BigOilEvent.class,
+        CasinoEvent.class,
+        ChampionshipWinEvent.class,
+        CurriculumChangeEvent.class,
+        EarthquakeEvent.class,
+        ExamWeekEvent.class,
+        FloodingEvent.class,
+        GraduationEvent.class,
+        GrantEvent.class,
+        LongboiDeathEvent.class,
+        PeacefulProtestEvent.class,
+        ScolarEvent.class,
+        StrikeEvent.class,
+        UniPartyEvent.class
+    );
+
+    for (Class<? extends Event> eventClass : events) {
+      try {
+        Event event = eventClass.getConstructor(GameScreen.class).newInstance(gameScreenMock).getEvent();
+        event.getOpt1().getAction().run();
+        if (event.hasChoice()) {
+          event.getOpt2().getAction().run();
+        }
+      } catch (Exception e) {
+        throw new RuntimeException("Failed to instantiate: " + eventClass.getSimpleName(), e);
+      }
+    }
   }
 }
